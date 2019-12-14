a5b00dcea1859e38524ded1e20be497e8f3f4b22
Miker Irick <miker@sandstormit.com>
2019-12-13 20:13:49 -0600

    removed sqlite3 file

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:michaelirick/wgu-capstone.git"
 
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'db/production.sqlite3')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 set :passenger_restart_with_touch, true
 set :rbenv_type, :user
