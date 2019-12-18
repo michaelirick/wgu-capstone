81bdc7748bf64bc3338be1670048a5c27d057b2c
Miker <michaelirick@gmail.com>
2019-12-17 18:15:19 -0600

    fixed image routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount Hyperstack::Engine => '/hyperstack'
-  get '/(*other)', to: 'hyperstack#app'
+  get '/', to: 'hyperstack#app'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
