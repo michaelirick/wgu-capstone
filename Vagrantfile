ccf58596ca1e340954897a89cc5e9747d7cdab38
Miker Irick <michaelirick@gmail.com>
2019-12-23 07:09:27 -0600

    vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "hashicorp/bionic64" # Ubuntu 18.4 LTS
   config.vm.hostname = 'cc-dev'
   config.vm.network :forwarded_port, guest: 5000, host: 5000
-  config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+  config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true, privileged: false
   config.vm.provider 'virtualbox' do |v|
     v.memory = ENV.fetch('CC_RAM', 2048).to_i
     v.cpus   = ENV.fetch('CC_CPU', 2).to_i
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
+su - vagrant
+cd /home/vagrant
+echo "Should be vagrant now"
+pwd
+
 # add keys for yarn/npm
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | -E apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-curl -sL https://deb.nodesource.com/setup_13.x | -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo -E apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+
+echo "Should be vagrant now"
+pwd
+
+sudo su - vagrant
 
 # install prerequisite libraries
-apt-get -y update
-apt-get -y install autoconf bison build-essential gcc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev git nodejs yarn postgresql libpq-dev libxml2 libxml2-dev libxslt1-dev libncurses5-dev
+sudo apt-get -y update
+sudo apt-get -y install autoconf bison build-essential gcc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev git nodejs yarn postgresql libpq-dev libxml2 libxml2-dev libxslt1-dev libncurses5-dev graphviz
+
+# setup database engine
+cat << EOF | sudo su - postgres -c psql
+CREATE ROLE vagrant WITH PASSWORD 'password' LOGIN SUPERUSER;
+EOF
+
+# test
+PGPASSWORDS=password psql -U vagrant postgres -c "\\l"
 
 # install ruby
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+git clone https://github.com/rbenv/rbenv.git /home/vagrant/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
+echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bashrc
+export PATH="/home/vagrant/.rbenv/bin:$PATH"
+export PATH="/home/vagrant/.rbenv/plugins/ruby-build/bin:$PATH"
 eval "$(rbenv init -)"
 mkdir -p "$(rbenv root)"/plugins
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 rbenv rehash
-rbenv install 2.6.5
+pwd
+rbenv install 2.4.1
 rbenv rehash
-rbenv global 2.6.5
+rbenv global 2.4.1
 gem install bundler
 
-# setup database engine
-cat << EOF | su - postgres -c psql
-CREATE ROLE vagrant WITH PASSWORD 'password' LOGIN SUPERUSER;
-EOF
+# setup rails
 cd /vagrant
 rm config/database.yml
 cp config/database.yml.example config/database.yml
-
-# setup rails
-bundle install
 yarn install
-bundle exec rails db:create db:migrate db:seed
+bundle install
+migrate=1 bundle exec rails db:create
+migrate=1 bundle exec rails db:migrate db:seed
+bundle exec rails webpacker:binstubs
