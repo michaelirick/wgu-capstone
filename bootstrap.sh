8af95e5fb32f1a31da6030cdad73c862704152b2
Miker Irick <miker.irick@sandstormit.com>
2019-12-23 13:45:04 -0600

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,18 +43,16 @@ rbenv rehash
 rbenv global 2.4.1
 gem install bundler
 
-# test yarn/webpack
-cd /vagrant
-yarn install --no-bin-links
-bin/webpack
-exit 1
 
 # setup rails
-cd /vagrant
+sudo cp -rf /vagrant /home/vagrant/app
+sudo chown -R vagrant:vagrant /home/vagrant/app
+cd /home/vagrant/app
 rm config/database.yml
 cp config/database.yml.example config/database.yml
-yarn install --no-bin-links
+yarn install
 bundle install
 migrate=1 bundle exec rails db:create
 migrate=1 bundle exec rails db:migrate db:seed
-bundle exec rails webpacker:binstubs
+bin/webpack
+# bundle exec rails webpacker:binstubs
